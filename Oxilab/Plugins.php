ff931c594b0ee588d22caa2e6625fc1f10035df7
Biplob Adhikari <50793821+MrOxizen@users.noreply.github.com>
2021-11-08 23:36:38 +0600

    rewrite Plugins

--- Oxilab/Plugins.php
+++ Oxilab/Plugins.php
@@ -70,7 +70,7 @@ class Plugins {
         ?>
         <div class="oxi-addons-wrapper">
             <div class="oxi-addons-import-layouts">
-                <h1>Oxilab Addons
+                <h1>Oxilab Plugins
                 </h1>
                 <p> We Develop Couple of plugins which will help you to Create Your Modern and Dynamic Websites. Just click and Install </p>
             </div>
